b678389327e45adc588e0491caf3ca0f34e56b07
Rashid MV <rashiidmv@gmail.com>
2019-06-23 19:54:08 +0530

    Browse  files user control is created

---
+++ Common/BrowseAndUploadView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Common {
+    /// <summary>
+    /// Interaction logic for BrowseAndUpload.xaml
+    /// </summary>
+    public partial class BrowseAndUploadView : UserControl {
+        public BrowseAndUploadView() {
+            InitializeComponent();
+            this.DataContext = new BrowseAndUploadViewModel();
+        }
+    }
+}
---
+++ Common/BrowseAndUploadViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Common {
+    public class BrowseAndUploadViewModel {
+        public BrowseAndUploadViewModel() {
+
+        }
+    }
+}
