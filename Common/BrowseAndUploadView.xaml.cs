4e2bdac56d93b58ce6e466cd1a92063d1c9fb31e
Rashid MV <rashiidmv@gmail.com>
2019-06-24 21:40:34 +0530

    Browse file user control implementation

--- Common/BrowseAndUploadView.xaml.cs
+++ Common/BrowseAndUploadView.xaml.cs
@@ -1,13 +1,68 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Common {
     /// <summary>
     /// Interaction logic for BrowseAndUpload.xaml
     /// </summary>
-    public partial class BrowseAndUploadView : UserControl {
+    public partial class BrowseAndUploadView : UserControl, INotifyPropertyChanged {
         public BrowseAndUploadView() {
             InitializeComponent();
-            this.DataContext = new BrowseAndUploadViewModel();
+        }
+
+        //public static readonly DependencyProperty SetTextProperty =
+        // DependencyProperty.Register("FilePathProperty", typeof(string), typeof(BrowseAndUploadView), new
+        //    PropertyMetadata("", new PropertyChangedCallback(OnSetTextChanged)));
+
+        //public string FilePath {
+        //    get { return (string)GetValue(FilePathProperty); }
+        //    set { SetValue(FilePathProperty, value); }
+        //}
+        public static readonly DependencyProperty FilePathProperty =
+                DependencyProperty.Register("FilePath", typeof(string), typeof(UserControl),
+                    new PropertyMetadata(string.Empty, OnCaptionPropertyChanged));
+
+        public string FilePath {
+            get { return (string)GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+        private static void OnCaptionPropertyChanged(DependencyObject dependencyObject,
+                       DependencyPropertyChangedEventArgs e) {
+            BrowseAndUploadView myUserControl = dependencyObject as BrowseAndUploadView;
+            myUserControl.OnPropertyChanged("FilePath");
+            myUserControl.OnCaptionPropertyChanged(e);
+        }
+        private void OnCaptionPropertyChanged(DependencyPropertyChangedEventArgs e) {
+            FileName.Text = FilePath;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+        public byte[] FileContent { get; private set; }
+        private void BrowseFileOpen_Click(object sender, System.Windows.RoutedEventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".jpg";
+            openFileDialog.Filter = "Images (.jpeg) | *.jpeg;*.png| All Files (*.*) | *.*";
+            openFileDialog.Multiselect = false;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if(result == true) {
+                FileName.Text = openFileDialog.FileName;
+                FileName.ToolTip = FileName.Text;
+                FilePath = FileName.Text;
+                FileContent = File.ReadAllBytes(FileName.Text);
+            }
+
         }
     }
 }
--- Marriage/MarriageCertificateViewModel.cs
+++ Marriage/MarriageCertificateViewModel.cs
@@ -1,11 +1,54 @@
 ﻿using MahalluManager.Infra;
+using Microsoft.Practices.Prism.Commands;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Marriage {
     public class MarriageCertificateViewModel : ViewModelBase {
+        public MarriageCertificateViewModel() {
+            InitializeDatePicker();
+            SaveMarriageCommand = new DelegateCommand(ExecuteSaveMarriageCommand);
+        }
+
+        
+
+        private DateTime startDate;
+        public DateTime StartDate {
+            get { return startDate; }
+            set {
+                startDate = value;
+                OnPropertyChanged("startDate");
+            }
+        }
+        private DateTime endDate;
+        public DateTime EndDate {
+            get { return endDate; }
+            set {
+                endDate = value;
+                OnPropertyChanged("endtDate");
+            }
+        }
+        private string bridePhoto;
+
+        public string BridePhoto {
+            get { return bridePhoto; }
+            set {
+                bridePhoto = value;
+                OnPropertyChanged("BridePhoto");
+            }
+        }
+        private DelegateCommand saveMarriageCommand;
+
+        public DelegateCommand SaveMarriageCommand {
+            get { return saveMarriageCommand; }
+            set { saveMarriageCommand = value; }
+        }
+        private void ExecuteSaveMarriageCommand() {
+            String s = BridePhoto;
+        }
+
+        private void InitializeDatePicker() {
+            StartDate = DateTime.Now.AddMonths(-2);
+            EndDate = DateTime.Now;
+        }
     }
 }
