d188a1dc9c6cf0128eb54c6886222074f9294ed4
Rashid MV <rashiidmv@gmail.com>
2019-07-10 13:21:32 +0530

    Marriage certificate implementation

--- Marriage/MarriageCertificateViewModel.cs
+++ Marriage/MarriageCertificateViewModel.cs
@@ -49,7 +49,7 @@ namespace Marriage {
         private string placeOfMarriage;
 
         public string PlaceOfMarriage {
-            get { return placeOfMarriage +"Moorad"; }
+            get { return placeOfMarriage + "Moorad"; }
             set {
                 placeOfMarriage = value;
                 OnPropertyChanged("PlaceOfMarriage");
@@ -227,93 +227,95 @@ namespace Marriage {
             PdfWriter writer = PdfWriter.GetInstance(document, fs);
             document.Open();
 
-
-            Chunk dateOfMarriage = new Chunk(MarriageDate.ToShortDateString());
-            dateOfMarriage.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f,BaseColor.BLACK);
-            
-
-            Chunk placeOfMarriage = new Chunk(PlaceOfMarriage);
-            placeOfMarriage.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.BLACK);
+            Paragraph title = new Paragraph();
+            title.Alignment = Element.ALIGN_CENTER;
+            title.Font = FontFactory.GetFont(FontFactory.HELVETICA, 20f, BaseColor.GREEN);
+            title.Add("MARRIAGE CERTIFICATE");
+            document.Add(title);
 
             Phrase phrase = new Phrase("This is from Phrase.");
             document.Add(phrase);
 
-            Image i = Image.GetInstance(GroomPhotoPath);
-            i.Alignment = Image.UNDERLYING;
-            i.ScaleToFit(100f, 160f);
-            i.SetAbsolutePosition(100, 360);
-            document.Add(i);
+            PdfContentByte pdfContentByte = writer.DirectContent;
+            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            pdfContentByte.SetColorFill(BaseColor.DARK_GRAY);
+            pdfContentByte.SetFontAndSize(bf, 14);
 
-            i = Image.GetInstance(BridePhotoPath);
-            i.Alignment = Image.UNDERLYING;
-            i.ScaleToFit(100f, 160f);
-            i.SetAbsolutePosition(100, 240);
-            document.Add(i);
+            if(!String.IsNullOrEmpty(GroomPhotoPath) && !String.IsNullOrEmpty(GroomPhotoPath)) {
+                Image i = Image.GetInstance(GroomPhotoPath);
+                i.Alignment = Image.UNDERLYING;
+                i.ScaleToFit(100f, 160f);
+                i.SetAbsolutePosition(200, 360);
+                pdfContentByte.AddImage(i);
+                i = Image.GetInstance(BridePhotoPath);
+                i.Alignment = Image.UNDERLYING;
+                i.ScaleToFit(100f, 160f);
+                i.SetAbsolutePosition(310, 360);
+                pdfContentByte.AddImage(i);
+            }
 
-            Paragraph para = new Paragraph("This is from paragraph.");
-            document.Add(para);
+            pdfContentByte.BeginText();
+            pdfContentByte.ShowTextAligned(1, GroomName, 520, 640, 0);
+            pdfContentByte.EndText();
 
-            Paragraph title = new Paragraph();
-            title.Alignment = Element.ALIGN_CENTER;
-            title.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.GREEN);
-            title.Add("MARRIAGE CERTIFICATE");
-            document.Add(title);
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(2, GroomFatherName, 100, 200, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(1, GroomHouseName, 230, 230, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(2, GroomArea, 150, 230, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            // pdfContentByte.ShowTextAligned(2, GroomPincode.ToString(), 150, 230, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(1, BrideName, 520, 640, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(2, BrideFatherName, 100, 200, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(1, BrideHouseName, 230, 230, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(2, BrideArea, 150, 230, 0);
+            //pdfContentByte.EndText();
+            //pdfContentByte.BeginText();
+            //pdfContentByte.ShowTextAligned(2, BridePinCode.ToString(), 150, 230, 0);
+            //pdfContentByte.EndText();
 
+            Chunk dateOfMarriage = new Chunk(MarriageDate.ToShortDateString());
+            dateOfMarriage.Font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14f, BaseColor.BLACK);
+            Chunk placeOfMarriage = new Chunk(PlaceOfMarriage);
+            placeOfMarriage.Font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14f, BaseColor.BLACK);
             Paragraph content = new Paragraph();
             content.SpacingBefore = 10;
             content.SpacingAfter = 10;
-            content.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.GREEN);
-            string contentText1 = "Certify that the Marriage between the above mentioned person was solemnised by Mahallu Khazi of Muhiyidheen Masjid, Iringal Moorad in the presence of their friends and relative ";
+            content.Font = FontFactory.GetFont(FontFactory.HELVETICA, 14f, BaseColor.BLACK);
+            string contentText1 = "        Certify that the Marriage between the above mentioned person was solemnised by Mahallu Khazi of Muhiyidheen Masjid, Iringal Moorad in the presence of their friends and relatives on ";
             string contentText2 = " at ";
-            string contentText3 = " according to the Islamic Shareeath Law";
+            string contentText3 = " according to the Islamic Shareeath Law.";
             content.Add(contentText1);
             content.Add(dateOfMarriage);
             content.Add(contentText2);
             content.Add(placeOfMarriage);
             content.Add(contentText3);
-            document.Add(content);
+            content.Alignment = Element.ALIGN_JUSTIFIED;
+            ColumnText ct = new ColumnText(writer.DirectContent);
+            ct.SetSimpleColumn(new Rectangle(30, 240, 560, 50));
+            ct.AddElement(content);
+            ct.Go();
 
-            // the pdf content
-            PdfContentByte cb = writer.DirectContent;
-            //cb.AddImage(i);
-            // select the font properties
-            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-            cb.SetColorFill(BaseColor.DARK_GRAY);
-            cb.SetFontAndSize(bf, 8);
+            Paragraph secretary = new Paragraph("SECRETARY");
+            secretary.Alignment = Element.ALIGN_RIGHT;
+            secretary.Font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14f, BaseColor.BLACK);
 
-            //// write the text in the pdf content
-            //cb.BeginText();
-            //cb.ShowTextAligned(1, GroomName, 520, 640, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(2, GroomFatherName, 100, 200, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(1, GroomHouseName, 230, 230, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(2, GroomArea, 150, 230, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            // cb.ShowTextAligned(2, GroomPincode.ToString(), 150, 230, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(1, BrideName, 520, 640, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(2, BrideFatherName, 100, 200, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(1, BrideHouseName, 230, 230, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(2, BrideArea, 150, 230, 0);
-            //cb.EndText();
-            //cb.BeginText();
-            //cb.ShowTextAligned(2, BridePinCode.ToString(), 150, 230, 0);
-            //cb.EndText();
-            //cb.AddTemplate(page, 0, 0);   
-            //page.Add(cb);
+            ct = new ColumnText(writer.DirectContent);
+            ct.SetSimpleColumn(new Rectangle(30, 140, 540, 30));
+            ct.AddElement(secretary);
+            ct.Go();
 
             document.Close();
             fs.Close();
@@ -321,7 +323,6 @@ namespace Marriage {
 
             String currentFolder = Path.GetDirectoryName(Application.ExecutablePath);
             CertificatePath = Path.Combine(currentFolder, "Certificate.pdf");
-
         }
     }
 }
